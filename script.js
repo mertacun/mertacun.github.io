42d72f76ee8169a503cc72e95ca8ce650b6be774
mertacun <acun0008@algonquinlive.com>
2024-05-10 22:05:53 -0400

    Update script.js: Add loader and overlay functionality, implement sticky header on scroll, and add hamburger menu toggle for mobile view

---
+++ script.js
@@ -0,0 +1,98 @@
+var loader = document.querySelector('.loader');
+var overlay = document.querySelector('.overlay');
+var header = document.querySelector('.header');
+
+window.addEventListener("load", function() {
+
+  setTimeout(function() {
+    overlay.style.opacity = '0';
+    loader.style.display = 'none';
+    header.style.display = 'flex';
+  }, 2000);
+
+  this.setTimeout(function() {
+    overlay.style.display = 'none';
+  }, 2500);
+});
+
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
+const navLinks = navMenu.querySelectorAll('li');
+
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('.header');
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle('active')
+  hamburger.classList.toggle('active')
+})
+
+function closeMenu() {
+  hamburger.classList.remove("active")
+  navMenu.classList.remove('active')
+}
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', closeMenu)
+})
+
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #FFD700}";
+  document.body.appendChild(css);
+};
